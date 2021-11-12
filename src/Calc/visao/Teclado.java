dccccf2ccc939a990c02fc96ad8f67590f03302d
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-11-12 07:55:09 -0300

    Add files via upload

---
+++ src/Calc/visao/Teclado.java
@@ -0,0 +1,12 @@
+package Calc.visao;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Teclado extends JPanel{
+	public Teclado() {
+		setBackground(Color.BLUE);
+	}
+}
